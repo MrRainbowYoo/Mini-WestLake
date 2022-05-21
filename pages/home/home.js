734a59d5c92dadfbccd48cf7adb8ab7749377808
MrRainbowYoo <813806051@qq.com>
2022-05-21 19:08:32 +0800

    solve the bugs of the map page

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,113 +1,33 @@
 Page({
     data: {
-        translateX: 0, // 位移x坐标 单位px
-        translateY: 100, // 位移y坐标 单位px
-        distance: 0, // 双指接触点距离
-        scale: 3, // 缩放倍数
-        startMove: { // 起始位移距离
-            x: 0,
-            y: 0,
-        },
-        startTouches: [], // 起始点touch数组,
-        lastTapDiffTime: 0,
-
-        latitude: 30.29149,
-        longitude: 120.01089,
+        imgSrc: 'https://obohe.com/i/2022/05/19/m192dx.jpg',
+        postions: [
+            {id: 0, left: 50, top: 50, name: '杭州站', content: 'this is position 0'},
+            {id: 1, left: 350, top: 100, name: '宁波站', content: 'this is position 1'},
+            {id: 2, left: 150, top: 250, name: '台州站', content: 'this is position 2'},
+            {id: 3, left: 200, top: 200, name: '嘉兴站', content: 'this is position 3'},
+        ],
+        highlightPos: 2
     },
-    touchStart(e) {
-        const touches = e.touches
-        const { translateX, translateY } = this.data
-        const { pageX, pageY } = touches[0]
-        this.data.startMove = {
-            x: pageX - translateX,
-            y: pageY - translateY
-        }
-        this.data.startTouches = touches
+    loadImg() {
+        wx.hideLoading()
     },
-    touchMove(e) {
-        const touches = e.touches
-        const { pageX: onePageX, pageY: onePageY } = touches[0]
-        const { startMove, scale, distance: oldDistance, startTouches } = this.data
-        if (touches.length === 2 && startTouches.length === 2) {
-            // 双指缩放
-            const { pageX: twoPageX, pageY: twoPageY } = touches[1]
-            // 求出当前双指距离
-            const distance = Math.sqrt((twoPageX - onePageX) ** 2 + (twoPageY - onePageY) ** 2)
-            this.data.distance = distance
-            this.setData({
-                scale: scale * (distance / (oldDistance || distance))
-            })
-        } else if (startTouches.length !== 2) {
-            // 单指拖拽
-            // this.setData({
-            //     translateX: onePageX - startMove.x,
-            //     translateY: onePageY - startMove.y
-            // })
-
-
-            var query = wx.createSelectorQuery();
-            query.select('#map-img').boundingClientRect((rect) => {
-                let { top, right, bottom, left, width, height } = rect
-                console.log(rect)
-                if(top > 0 || left > 0 || right < 375 || bottom < 543) {
-                    console.log('越界')
-                    console.log(onePageY - startMove.y)
-                    // this.setData({
-                    //     translateX: onePageX - startMove.x,
-                    //     translateY: onePageY - startMove.y
-                    // })
-                } else {
-                    this.setData({
-                        translateX: onePageX - startMove.x,
-                        translateY: onePageY - startMove.y
-                    })
-                }
-            }).exec()
-        }
-    },
-    touchEnd() {
-        this.data.distance = null;
-    },
-    initMap() {
-        this.data = {
-            translateX: 0, // 位移x坐标 单位px
-            translateY: 0, // 位移y坐标 单位px
-            distance: 0, // 双指接触点距离
-            scale: 1, // 缩放倍数
-            startMove: { // 起始位移距离
-                x: 0,
-                y: 0,
-            },
-            startTouches: [] // 起始点touch数组
-        }
+    clickPostion(e) {
+        let { id: clickId } = e.currentTarget.dataset
+        this.setData({
+            highlightPos: clickId
+        })
+        let currentPostion = this.data.postions.find(item => item.id === clickId)
+        let content = currentPostion.content
+        console.log(content)
     },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-        this.width = wx.getSystemInfoSync().windowWidth;
-
-        this.mapCtx = wx.createMapContext('map')
-        this.mapCtx.addGroundOverlay({
-            id: 0,
-            src: "https://obohe.com/i/2022/05/19/lpxwij.png",
-            bounds: {
-                southwest: { //西南角
-                    latitude: 30.285826,
-                    longitude: 120.007203,
-                },
-                northeast: { //东北角
-                    latitude: 30.299964,
-                    longitude: 120.02327,
-                }
-            },
-            success(res) {
-                console.log('wp', res)
-            },
-            fail(err) {
-                console.log('wperr', err)
-            }
-        })
+        wx.showLoading({
+            title: '加载中......'
+          });
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
